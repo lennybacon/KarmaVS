5ea47480f62c7fcca0e8b3131c20a644fd0fdc10
Lennybacon <info@lennybacon.com>
2013-09-27 17:23:41 +0200

    F: Added project item template

--- src/SampleWebApplication/karma start.cmd
+++
@@ -1,5 +0,0 @@
-SET CHROME_BIN=C:\Users\Daniel\AppData\Local\Google\Chrome\Application\chrome.exe
-
-"C:\Program Files\nodejs\node.exe" C:\Users\Daniel\AppData\Roaming\npm\node_modules\karma\bin\karma start .\karma.conf.js
-
-pause
